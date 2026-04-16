30c314b3c14f15d5ec916c515308e73073811112
mehul tiwari <161856797+MehulTiwari007@users.noreply.github.com>
2026-04-16 22:08:18 +0530

    Without generics, collections store Object, so explicit casting is required."

---
+++ src/List/ArrayList/ArrayList5.java
@@ -0,0 +1,14 @@
+package List.ArrayList;
+import java.util.*;
+
+class ArrayList5 {
+    public static void main(String[] args) {
+        ArrayList al = new ArrayList();
+        al.add(20);
+        al.add(10);
+
+        for (Integer i : al) {
+            System.out.println(i);
+        }
+    }
+}
