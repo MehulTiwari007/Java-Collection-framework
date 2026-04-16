74a88e03364e3b1eaadc95b6f1e4f6239857072e
mehul tiwari <161856797+MehulTiwari007@users.noreply.github.com>
2026-04-16 16:37:06 +0530

    Basic example of ArrayList

---
+++ src/List/ArrayList/ArrayList1.java
@@ -0,0 +1,14 @@
+package List.ArrayList;
+import java.util.*;
+public class ArrayList1 {
+    public static void main(String ar[])
+    {
+        List l1=new ArrayList();
+        l1.add("ram");
+        l1.add("sita");
+        l1.add("laskshman");
+        System.out.println(l1);
+    }
+
+
+}
