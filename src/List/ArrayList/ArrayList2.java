27e8331fca3755a0d1b03ba6a7d81299a6131a40
mehul tiwari <161856797+MehulTiwari007@users.noreply.github.com>
2026-04-16 17:05:51 +0530

    NULL INSERTION IS ALLOWED IN ARRAYLIST

---
+++ src/List/ArrayList/ArrayList2.java
@@ -0,0 +1,17 @@
+package List.ArrayList;
+import java.util.*;
+public class ArrayList2 {
+    public static void main(String ar[])
+    {
+        ArrayList l1=new ArrayList();
+        l1.add("ram");
+        l1.add("sita");
+        l1.add("laskshman");
+        l1.add(" 123");
+        l1.add(1);
+        l1.add(null);
+        System.out.println(l1);
+    }
+
+
+}
