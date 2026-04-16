3c7cf214a5b9d485bfc025066845fae104f23b0d
mehul tiwari <161856797+MehulTiwari007@users.noreply.github.com>
2026-04-16 21:45:30 +0530

    This code will not work in ver1.4 because there is no autoboxing till ver 1.4

---
+++ src/List/ArrayList/ArrayList4.java
@@ -0,0 +1,12 @@
+package List.ArrayList;
+import java.util.*;
+class ArrayList4
+{
+    public static void main(String ar[])
+    {
+        ArrayList al = new ArrayList();
+        al.add(new Integer(11));
+        al.add(new Integer(10));
+        System.out.println(al);
+    }
+}
