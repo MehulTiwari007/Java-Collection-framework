fdb454568ca12d5699b68b929ac9b211e9eb4b82
mehul tiwari <161856797+MehulTiwari007@users.noreply.github.com>
2026-04-16 21:42:28 +0530

    This code will not work in ver1.4

---
+++ src/List/ArrayList/ArrayList3.java
@@ -0,0 +1,11 @@
+package List.ArrayList;
+import java.util.*;
+class ArrayList3
+{
+    public static void main(String ar[])
+    {
+        ArrayList al=new ArrayList();
+        al.add(20);
+        al.add(10);
+        System.out.println(al);
+    }}
