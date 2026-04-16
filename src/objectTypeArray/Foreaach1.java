43d803897c5f4ee8f3ae95c4d70a78e2fb51fdb1
mehul tiwari <161856797+MehulTiwari007@users.noreply.github.com>
2026-04-16 14:59:11 +0530

    Added foreach loop example in ObjectTypeArray package and for eac is only applicable to iterator type

---
+++ src/objectTypeArray/Foreaach1.java
@@ -0,0 +1,15 @@
+
+package ObjectTypeArray;
+
+    public class Foreaach1 {
+        public static void main(String ar[])
+        { int x=0;
+            for(int x1:x)
+            {
+                System.out.println(x1);
+            }
+        }
+
+    }
+
+
